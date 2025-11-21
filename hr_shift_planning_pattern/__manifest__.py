dccc884b4321944d1b2ff3cfdbaf34b517927454
jesus <appevolutioncenter@gmail.com>
2025-11-21 11:53:25 +0100

    fixes

--- hr_shift_planning_pattern/__manifest__.py
+++ hr_shift_planning_pattern/__manifest__.py
@@ -11,6 +11,7 @@
         "views/hr_shift_planning_pattern_views.xml",
         "views/hr_shift_planning_pattern_menu.xml",
         "views/hr_employee_views.xml",
+        'views/hr_shift_views.xml',
     ],
     "installable": True,
     "application": False,
