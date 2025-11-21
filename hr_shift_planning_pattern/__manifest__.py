a82d77e6bf6cf38bd69550fd106d00ba2e733d60
jesus <appevolutioncenter@gmail.com>
2025-11-21 10:35:45 +0100

    fixes

--- hr_shift_planning_pattern/__manifest__.py
+++ hr_shift_planning_pattern/__manifest__.py
@@ -1,16 +1,15 @@
 {
     "name": "HR Shift Planning Patterns",
-    "version": "17.0.1.0.0",
-    "summary": "Add cyclic shift patterns (6+2, 2M/2T/2N/2D, etc.)",
+    "version": "17.0.1.0.1",
+    "summary": "Add cyclic shift patterns",
     "category": "Human Resources",
-    "author": "Jesús Río / Fork",
+    "author": "Jesús Río",
     "license": "AGPL-3",
     "depends": ["hr", "hr_shift"],
     "data": [
         "security/ir.model.access.csv",
         "views/hr_shift_planning_pattern_views.xml",
         "views/hr_shift_planning_pattern_menu.xml",
-        # "data/cron_data.xml",   # opcional, si luego quieres auto-generar turnos
     ],
     "installable": True,
     "application": False,
